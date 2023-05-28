19b3b989bda520a88d2347fd16ca2d551564fc8c
AmeyShahane <ameyshahane1012@gmail.com>
2023-05-27 20:48:21 -0400

    Added mongoDB Connection

---
+++ backend/models/book.model.js
@@ -0,0 +1,28 @@
+const { Schema, default: mongoose } = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    username : {
+        type : String,
+        required : true,
+    },
+    description : {
+        type : String,
+        required : true,
+    },
+    duration : {
+        type : Number,
+        required : true
+    },
+    date : {
+        type : Date,
+        required : true
+    }
+},{
+    timestamps : true,
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
